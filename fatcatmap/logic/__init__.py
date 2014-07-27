afff77a0ea40fdc9762ff4c774727ecab7599f88
Sam Gammon <sam.gammon@causes.com>
2014-07-26 22:15:38 -0700

    Pulling in Views logic to avoid merge conflict with module init.

--- fatcatmap/logic/__init__.py
+++ fatcatmap/logic/__init__.py
@@ -6,12 +6,16 @@
 
 '''
 
+# views
+from . import views
+
 # grapher
 from . import grapher
 from .grapher import *
 
 
 __all__ = (
+  'views',
   'grapher',
   'Graph',
   'Grapher',
--- fatcatmap/services/content/service.py
+++ fatcatmap/services/content/service.py
@@ -6,21 +6,41 @@
 
 '''
 
-# messages
+# fcm
 from . import messages
+from . import exceptions
 
 # RPC APIs
 from canteen import rpc
 
 
-@rpc.public('content')
+@rpc.remote.service('content')
 class ContentService(rpc.Service):
 
   '''  '''
 
-  @rpc.public(messages.ContentRequest, messages.GeneratedContent)
   def generate(self, request):
 
     '''  '''
 
     pass
+
+  @rpc.remote.method(messages.TemplateRequest, messages.ClientTemplate)
+  def template(self, request):
+
+    ''' Retrieves a client-side template. '''
+
+    if not request:
+      raise self.exceptions.bad_request('No request received.')
+
+    if not request.path:
+      raise self.exceptions.bad_request('Template requests require a path to retrieve.')
+
+    try:
+      return messages.ClientTemplate(**{
+        'source': self.views.load_template(request.path),
+        'path': request.path
+      }).to_message()
+    except ValueError:
+      # template was invalid
+      raise self.exceptions.template_not_found('Couldn\'t find template ' + request.path)
