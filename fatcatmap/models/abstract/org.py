36bf929b37d716a51e0d38f42a220d881de1a921
Sam Gammon <sam@momentum.io>
2014-08-25 12:22:48 -0700

    Introducing executive government models.

--- fatcatmap/models/abstract/org.py
+++ fatcatmap/models/abstract/org.py
@@ -13,3 +13,18 @@ from .group import Group
 class Organization(Group):
 
   '''  '''
+
+
+class Institution(Organization):
+
+  '''  '''
+
+
+class Corporation(Organization):
+
+  '''  '''
+
+
+class Government(Organization):
+
+  '''  '''
