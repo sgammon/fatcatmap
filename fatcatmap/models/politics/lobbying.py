f95d1ae1369e9ec34fbd88039b34a3b77236fd7a
Sam Gammon <sam@momentum.io>
2014-09-14 06:23:08 -0700

    Finished up lobbying models for FCM-69.

--- fatcatmap/models/politics/lobbying.py
+++ fatcatmap/models/politics/lobbying.py
@@ -12,11 +12,13 @@ from .. import (Vertex,
 
 # abstract models
 from ..abstract import (Role,
+						Event,
 						Contract)
 
 # fcm models
 from ..person import Person
 from ..commercial.business import Corporation
+from ..government.legislative import Legislation
 
 
 ## +=+=+=+=+=+=+=+=+ Vertexes +=+=+=+=+=+=+=+=+ ##
@@ -24,13 +26,17 @@ from ..commercial.business import Corporation
 @describe(parent=Corporation, type=Role)
 class LobbyingFirm(Corporation):
 
-  '''  '''
+  ''' Specifies a firm that employs ``Lobbyists`` and is
+  	  retained by clients to advocate for them on issues
+  	  and legislation. '''
 
 
 @describe(parent=Person, type=Role)
 class Lobbyist(Vertex):
 
-  '''  '''
+  ''' Specifies an individual playing a role where they
+      are trying to influence government on their own
+      behalf or for someone/something else. '''
 
   firm = LobbyingFirm, {'indexed': True, 'default': None, 'embedded': False}
 
@@ -40,6 +46,14 @@ class Lobbyist(Vertex):
 @describe(type=Contract)
 class Retains(Corporation >> LobbyingFirm):
 
-  '''  '''
+  ''' Specifies a relationship between a ``Corporation``
+  	  and a ``LobbyingFirm`` where the firm is retained
+  	  to advocate on their behalf. '''
 
-  pass
+
+@describe(type=Event)
+class Lobbying(Lobbyist >> (Person, Legislation)):
+
+  ''' Specifies activity performed by a ``Lobbyist``,
+      sometimes under the banner of a ``LobbyingFirm``
+      but always on behalf of a client. '''
