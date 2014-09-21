ea04d2a7824eb73698ce19302bd435f9a00bd086
Ian Weisberger <ian@thegridhost.com>
2014-09-21 06:37:13 -0700

    added search indexer

---
+++ fatcatmap/logic/search/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+'''
+
+  fcm: search logic
+
+'''
+
+# submodules
+from . import driver
+from . import indexer
+
+# subsymbols
+from .driver import *
+from .indexer import *
+
+__all__ = ('driver','indexer',)
