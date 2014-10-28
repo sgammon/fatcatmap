91949c025a57eaf08743116e23b421ed15502c18
Sam Gammon <sam@momentum.io>
2014-10-28 01:21:53 -0700

    Initial commit of fatcatmap for iOS :)

---
+++ apps/ios/fatcatmap/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  fatcatmap
+//
+//  Created by Sam Gammon on 10/27/14.
+//  Copyright (c) 2014 momentum labs. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
